3cb82f06e043e00d8cd5c612770a4dc6df5719a3
Amen AMOUZOU <amouzou@gmail.com>
2018-03-17 12:42:33 +0000

    Add firebase ref

--- js/app.js
+++ js/app.js
@@ -29,33 +29,60 @@ function Event() {
 }
 
 
+
+// Sets up shortcuts to Firebase features and initiate firebase auth.
 Event.prototype.initFirebase = function() {
-   
+    this.auth = firebase.auth();
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+    // Initiates Firebase auth and listen to auth state changes.
+    this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
 
 
+
+// Signs-in Friendly Chat.
 Event.prototype.signIn = function() {
-  
+  var GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+  // var provider = new firebase.auth.FacebookAuthProvider();
+  this.auth.signInWithPopup(GoogleAuthProvider);
 };
 
-
+// Signs-out of Friendly Chat.
 Event.prototype.signOut = function() {
-
+  // TODO(DEVELOPER): Sign out of Firebase.
+  this.auth.signOut();
+  // document.location.reload(true);
 };
 
-
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
 Event.prototype.onAuthStateChanged = function(user) {
-  if (user) {
+  if (user) { // User is signed in!
     this.dashboard.removeAttribute('hidden');
     this.loginPage.setAttribute('hidden', 'true');
 
-  } else {
+
+    // console.log("Logged as ", user, this);
+
+  } else { // User is signed out!
     this.loginPage.removeAttribute('hidden');
     this.dashboard.setAttribute('hidden', 'true');
   }
 };
 
 Event.prototype.loadDashbord = function() {
+    this.eventsRef = this.database.ref('events');
+    // Make sure we remove all previous listeners.
+    this.eventsRef.off();
+
+    // Loads the first 2 events with  statut=true and listen for new ones.
+    var setEvent = function(data) {
+    var val = data.val();
+        this.displayEvent(data.key, val.name, val.date, val.detail);
+        
+    }.bind(this);
+    this.eventsRef.orderByChild("statut").limitToFirst(2).equalTo(true).on('child_added', setEvent);
+    this.eventsRef.orderByChild("statut").limitToFirst(2).equalTo(true).on('child_changed', setEvent);
 
 };
 
@@ -66,13 +93,34 @@ Event.MESSAGE_TEMPLATE =
                             '<div>'+
                                 '<span class="h4 name d-block font-weight-normal mb-2"></span>' +
                             '</div>' +
-                            '<div class="detail"></div>'+
+                            '<div class="detail">loremloremlorem</div>'+
                         '</div>' +
                     '</div>';
 
+Event.prototype.displayEvent = function(key, name, date, detail) {
+    console.log("displayEvent==>", key, name, date, detail);
+    
+    this.accueil.removeAttribute('hidden');
+    this.accueil.querySelector(".row").textContent = this.MESSAGE_TEMPLATE;
+    container.innerHTML = FriendlyChat.MESSAGE_TEMPLATE;
+    div = container.firstChild;
+    
+    $("#accueil").removeAttr("hidden");
+    $("#accueil row").append( "toto" );
+
+    // alert($("#accueil").removeAttr("hidden").find(".row").length);
+    // .find(".card:last")
+    //     .find(".name").text(name).end()
+    //     .find(".detail").html(detail).end()
+    
+    
+};
+
 
 Event.prototype.addEvent = function(event) {
+  // TODO(DEVELOPER): Sign out of Firebase.
   event.preventDefault();
+
 
   if(this.eventName.value.length && this.eventDate.value.length){
     let data = {
@@ -81,17 +129,22 @@ Event.prototype.addEvent = function(event) {
                     detail: this.eventDetail.value,
                     statut: false
                 };
-    // firebase.database().ref('/').child("events").push(data).then(_=> {
-    //     Snackbar.show({text: 'Evènement <u>' + data.name + '</u> enregistré avec succès !', actionText: 'Fermer', pos: 'top-center'});
-    //     this.eventAddForm.reset();
-    // });
+    firebase.database().ref('/').child("events").push(data).then(_=> {
+        Snackbar.show({text: 'Evènement <u>' + data.name + '</u> enregistré avec succès !', actionText: 'Fermer', pos: 'top-center'});
+        this.eventAddForm.reset();
+    });
 
-    
+    console.log("Send", this.eventName.value, this.eventDate.value, this.eventDetail.value);
   }
-  
+  else{
+    console.log("Some errors");
+  }
+
   
 };
 
+
+// Checks that the Firebase SDK has been correctly setup and configured.
 Event.prototype.checkSetup = function() {
   if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
     window.alert("Vous n'avez pas encore configuré ni importé le SDK Firebase. ");
@@ -100,4 +153,6 @@ Event.prototype.checkSetup = function() {
 
 window.onload = function() {
   window.Event = new Event();
+  Event.loadDashbord();
 };
+
